8fca532e7d011213c0fdebacc394d4fda0a10057
AMAN110319 <98185335+AMAN110319@users.noreply.github.com>
2023-05-31 19:05:56 +0530

    first commit
    this contains several react projects

---
+++ react_app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Resturant from "./component/basics/Resturant"; 
+// import UseState from "./component/hooks/useState";
+// import UseEffect from "./component/hooks/useEffect";
+// import UseReducer from "./component/hooks/useReducer";
+// import Todo from "./component/todoreact/todo";
+// import Temp from "./component/weather/temp";
+const App = () => {
+  return <Resturant />;
+};
+
+export default App;
